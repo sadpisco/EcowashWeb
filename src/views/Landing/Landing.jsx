2ae183e5ea5c7c41177508211daa069f6c1e7409
Leonardo Risco <leo99153@gmail.com>
2024-04-13 00:26:25 -0500

    html

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 export default function Landing(){
     return(
         <div>
+            <header>
+            </header>
             Hello from Landing
         </div>
     )
