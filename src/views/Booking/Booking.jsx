5f6db6bcba0a89388a5363f1eb15fc8cec566c01
Leonardo Risco <leo99153@gmail.com>
2024-04-23 15:59:49 -0500

    calendly installed

--- src/views/Booking/Booking.jsx
+++ src/views/Booking/Booking.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../../components/Footer/Footer';
 import NavBar from '../../components/NavBar/NavBar';
+import Calendly from '../../components/Calendly/Calendly';
 
 export default function Booking(){
     return(
@@ -9,7 +10,7 @@ export default function Booking(){
                 <NavBar />
             </header>
             <body className = "bg-slate-800  h-auto w-full flex flex-col items-start justify-center">
-                <h1>Reservar Cita</h1>
+                <Calendly />
             </body>
             <footer className = "w-full">
                 <Footer />
