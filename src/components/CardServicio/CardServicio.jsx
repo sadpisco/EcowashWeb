8f3d0a2d8b0a44d2dfe38a0202d40e36a57ea216
Leonardo Risco <leo99153@gmail.com>
2024-05-04 02:53:54 -0500

    responsiveness from services is done

--- src/components/CardServicio/CardServicio.jsx
+++ src/components/CardServicio/CardServicio.jsx
@@ -3,32 +3,32 @@ import styles from './CardServicio.module.css';
 
 export default function CardServicio(service){
     return(
-        <div className = "w-[1000px] max-w-[90%] h-auto min-h-[380px] flex flex-row items-center justify-center rounded-lg">
-            <img src = {service.service.img} alt = {service.service.nombre} className = "w-[300px] h-[380px] shadow-2xl rounded-lg"/>
-            <section className = "pl-8 pr-8 flex flex-col items-center justify-around h-[380px] bg-slate-900 rounded-lg">
-            <h1 className = "font-white font-montserrat font-semibold text-4xl text-white">{service.service.nombre}</h1>
-             <p className = "font-montserrat font-normal text-xl text-white">{service.service.descripcion}</p>
-             <section className = "flex flex-row items-center justify-around  w-[70%] h-auto min-h-[60px]">
-                <div className = "bg-slate-200  rounded-sm h-auto flex flex-col items-center justify-center text-black   font-montserrat shadow-xl p-4" >
-                    <h4 className = "font-montserrat text-lg font-semibold">Auto</h4>
-                    <h6 className = "font-montserrat font-medium text-lg">S/. {service.service.auto}</h6>
-                </div>
-                <div className = "bg-slate-200  rounded-sm h-auto flex flex-col items-center justify-center text-black   font-montserrat shadow-xl p-4">
-                    <h4 className = "font-montserrat text-lg font-semibold">SUV</h4>
-                    <h6 className = "font-montserrat font-medium text-lg">S/. {service.service.suv}</h6>
-                </div>
-                <div className = "bg-slate-200  rounded-sm h-auto flex flex-col items-center justify-center text-black   font-montserrat shadow-xl p-4">
-                    <h4 className = "font-montserrat text-lg font-semibold">4x4</h4>
-                    <h6 className = "font-montserrat font-medium text-lg">S/. {service.service.forbyfor}</h6>
-                </div>
-                <div className = "bg-slate-200  rounded-sm h-auto flex flex-col items-center justify-center text-black   font-montserrat shadow-xl p-4">
-                    <h4 className = "font-montserrat text-lg font-semibold">Van</h4>
-                    <h6 className = "font-montserrat font-medium text-lg">S/. {service.service.van}</h6>
-                </div>
-             </section>
+        <div className="w-[95%] max-w-[700px] h-auto min-h-[380px] flex flex-col items-center justify-center rounded-lg  xl:flex-row gap-[2vh]">
+            <img src={service.service.img} alt={service.service.nombre} className="w-[auto] max-w-[300px] h-[auto] shadow-4xl rounded-lg" />
+            <section className="gap-[2vh] pl-4 pr-4 flex flex-col items-center justify-around h-[auto]  rounded-lg w-[100%]">
+                <h1 className="font-white font-montserrat font-semibold text-2xl text-white text-center lg:text-4xl">{service.service.nombre}</h1>
+                <p className="font-montserrat font-normal text-md text-white text-center lg:text-lg">{service.service.descripcion}</p>
+                <section className="flex flex-row items-center justify-around  w-[auto] h-auto min-h-[60px] bg-slate-200 shadow-xl">
+                    <div className=" rounded-sm h-auto flex flex-col items-center justify-center text-black   font-montserrat  p-4" >
+                        <h4 className="font-montserrat text-md font-semibold lg:text-lg">Auto</h4>
+                        <h6 className="font-montserrat font-medium text-md lg:text-lg">S/. {service.service.auto}</h6>
+                    </div>
+                    <div className=" rounded-sm h-auto flex flex-col items-center justify-center text-black   font-montserrat  p-4">
+                        <h4 className="font-montserrat text-md font-semibold lg:text-lg">SUV</h4>
+                        <h6 className="font-montserrat font-medium text-md lg:text-lg">S/. {service.service.suv}</h6>
+                    </div>
+                    <div className="  rounded-sm h-auto flex flex-col items-center justify-center text-black   font-montserrat  p-4">
+                        <h4 className="font-montserrat text-md font-semibold lg:text-lg">4x4</h4>
+                        <h6 className="font-montserrat font-medium text-md lg:text-lg">S/. {service.service.forbyfor}</h6>
+                    </div>
+                    <div className="  rounded-sm h-auto flex flex-col items-center justify-center text-black   font-montserrat p-4">
+                        <h4 className="font-montserrat text-md font-semibold lg:text-lg">Van</h4>
+                        <h6 className="font-montserrat font-medium text-md lg:text-lg">S/. {service.service.van}</h6>
+                    </div>
+                </section>
 
             </section>
-             
+
         </div>
     )
 }
--- src/views/Services/Services.jsx
+++ src/views/Services/Services.jsx
@@ -43,38 +43,40 @@ export default function Services(){
             </header>
             <section className = "bg-slate-800  h-auto w-full flex flex-col items-start justify-center">
                 <section className = {`${styles.divOne} w-full flex flex-col items-center justify-center h-[350px]`}>
-                <h1 className = "font-montserrat font-semibold text-white">Nuestros Servicios</h1>
+                <h1 className = "font-montserrat font-semibold text-whit text-center">Nuestros Servicios</h1>
                 </section>
-                <section className = "w-full flex flex-col items-center justify-center h-auto p-10 bg-gradient-to-r from-black to-slate-500 ">
+                <section className = " flex flex-col justify-center p-4 h-full w-full items-center bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] ">
                     {service && <CardServicio service = {service}/>}
                     <div className = "flex flex-row items-center justify-center gap-[2vh] pt-[2vh]">
-                    <button className = "bg-slate-200 p-0 rounded-md h-[60px] w-[150px] text-black flex items-center justify-center font-montserrat font-semibold text-lg shadow-xl" onClick={prevSlide}><NavigateBeforeIcon />Anterior</button>
-                    <button className = "bg-slate-200 p-0  rounded-md h-[60px] w-[150px] text-black flex items-center justify-center font-montserrat font-semibold text-lg shadow-xl" onClick={nextSlide}>Siguiente<NavigateNextIcon /></button>
+                    <button className = "bg-slate-200 p-0 rounded-md h-[60px] min-w-[150px] text-black flex items-center justify-center font-montserrat font-semibold text-lg shadow-xl" onClick={prevSlide}><NavigateBeforeIcon />Anterior</button>
+                    <button className = "bg-slate-200 p-0  rounded-md h-[60px] min-w-[150px] text-black flex items-center justify-center font-montserrat font-semibold text-lg shadow-xl" onClick={nextSlide}>Siguiente<NavigateNextIcon /></button>
                     </div>
                 </section>
-                <section className = "w-full flex flex-col items-center justify-center p-[4vh] h-auto bg-gradient-to-r from-slate-700 to-slate-900 ">
-                    <h1 className = "font-montserrat font-semibold w-full text-center text-white">Otros Servicios</h1>
-                    <div className = "bg-gray h-[80%] w-[90%] flex items-center justify-around p-[4vh]">
-                        <div className = "flex flex-col items-center gap-8 justify-around w-[30%] ">
+                <div className = "h-full w-full bg-fuchsia-400 bg-[size:20px_20px] opacity-100 blur-[0px]">
+                <section className = "w-full flex flex-col items-center justify-center p-[4vh] h-auto bg-neutral-900">
+                    <h1 className = "font-montserrat font-semibold w-full text-center text-white text-3xl lg:text-4xl">Otros Servicios</h1>
+                    <div className = "bg-gray h-[80%] w-[90%] flex flex-col lg:flex-row items-center justify-around p-[4vh] gap-[6vh]">
+                        <div className = "flex flex-col items-center gap-8 justify-around w-[100%] max-w-[400px] lg:w-[30%]">
                             <img src = {descontaminadoImg} className = "h-[200px] rounded-lg border-4 border-white shadow-2xl"/>
-                            <h4 className = "font-montserrat text-center text-2xl font-bold text-white">Descontaminación de carrocería</h4>
+                            <h4 className = "font-montserrat text-center text-xl lg:text-2xl font-bold text-white">Descontaminación de carrocería</h4>
                             <p className = "font-montserrat text-center text-lg font-medium text-white">Limpieza profunda y detallada utilizando diferentes técnicas y productos para lograr eliminar aquellas partículas que se incrustan en la superficie del coche, sin perjudicar a la pintura ni rayarlo.</p>
                             <p className = "text-black font-montserrat font-semibold text-xl bg-slate-200 p-4 rounded-md">S/. 80</p>
                         </div>
-                        <div className = "flex flex-col items-center gap-8 justify-around w-[30%] ">
+                        <div className = "flex flex-col items-center gap-8 justify-around w-[100%] max-w-[400px] lg:w-[30%]">
                         <img src = {carroceriaImg} className = "h-[200px] rounded-lg border-4 border-white shadow-2xl"/>
-                            <h4 className = "font-montserrat text-center text-2xl font-bold text-white">Tratamiento de pintura</h4>
+                            <h4 className = "font-montserrat text-center text-xl lg:text-2xl font-bold text-white">Tratamiento de pintura</h4>
                             <p className = " font-montserrat text-center text-lg font-medium text-white">Recubrimientos cerámicos o cerámicos líquidos de alta calidad son aplicados a la pintura del vehiculo. Estos recubrimientos proporcionan una capa duradera y resistente que protege la pintura contra los rayos UV, los contaminantes ambientales, los arañazos y la decoloración.</p>
                             <p className = "text-black font-montserrat font-semibold text-xl bg-slate-200 p-4 rounded-md">S/. 180</p>
                         </div>
-                        <div className = "flex flex-col items-center gap-8 justify-around w-[30%] ">
+                        <div className = "flex flex-col items-center gap-8 justify-around w-[100%] max-w-[400px] lg:w-[30%]">
                         <img src = {pulidoImg} className = "h-[200px] rounded-lg border-4 border-white shadow-2xl"/>
-                            <h4 className = "font-montserrat text-center text-2xl font-bold text-white">Pulido de faros</h4>
+                            <h4 className = "font-montserrat text-center text-xl lg:text-2xl font-bold text-white">Pulido de faros</h4>
                             <p className = "font-montserrat text-center text-lg font-medium text-white">Es uno de los efectos más acusados del paso del tiempo en los vehículos, incluso entre aquellos mejor cuidados por sus conductores. Los faros son pulidos utilizando productos especializados para ello y priorizar la presencia de tu vehiculo.</p>
                             <p className = "text-black font-montserrat font-semibold text-xl bg-slate-200 p-4 rounded-md">S/. 60</p>
                         </div>
                     </div>
                 </section >
+                </div>
             
             </section>
             <footer className = "w-full">
