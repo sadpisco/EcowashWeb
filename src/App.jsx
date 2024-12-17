2f934b855b78d810ea8106d0906bd4c34bbbfc17
sadpisco <leo99153@gmail.com>
2024-12-16 23:30:11 -0500

    Now account is ruling

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Routes, Route} from 'react-router-dom';
-import Landing from './views/Landing/Landing';
-import Booking from './views/Booking/Booking';
-import Nosotros from './views/Nosotros/Nosotros';
-import Services from './views/Services/Services';
-import Contact from './views/Contact/Contact';
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./views/Landing/Landing";
+import Booking from "./views/Booking/Booking";
+import Nosotros from "./views/Nosotros/Nosotros";
+import Services from "./views/Services/Services";
+import Contact from "./views/Contact/Contact";
 
 function App() {
   return (
-    <div className = 'App'>
+    <div className="App">
       <Routes>
-        <Route path = '/' element = {<Landing />}/>
-        <Route path = '/booking' element = {<Booking />}/>
-        <Route path = '/nosotros' element = {<Nosotros />}/>
-        <Route path = '/services' element = {<Services />}/>
-        <Route path = '/contact' element = {<Contact />}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-
     </div>
-  )
+  );
 }
-export default App
+export default App;
